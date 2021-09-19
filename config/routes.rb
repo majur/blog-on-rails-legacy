0a3fc34b35ca01b5e0ba0d5011e8e9bc5750ed29
majur <juraj@matase.sk>
2021-09-19 22:54:32 +0200

    Adding articles to blog view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   delete '/users',   to: 'users#destroy'
   get    '/logout',  to: 'sessions#new'
   get    '/signup',  to: 'users#new'
+  # get    '/',        to: 'posts#index'
+  # get    '/',        to: 'blog#blog'
   resources :posts
   resources :users
   root 'blog#blog'
