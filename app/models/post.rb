2b7eca00bc2c14226984dda5cb03751a8d36baa0
majur <juraj@matase.sk>
2021-09-23 16:55:24 +0200

    Adding trix editor to posts content

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   # belongs_to :user
+  has_rich_text :content
   default_scope -> { order(created_at: :desc) }
   # validates :user_id, presence: true
   validates :content, presence: true
