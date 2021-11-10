1108a23ddab047e8dcb8cece06e5ec2a2802fab4
majur <juraj@matase.sk>
2021-11-10 20:16:38 +0100

    Adding sorting for manu items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get    'menu/index'
   get    '/login',   to: 'sessions#new'
   post   '/signup',  to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
